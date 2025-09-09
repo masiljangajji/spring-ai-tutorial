c46a489140dc7100df836be30d99434bb117ef75
masiljangajji <xmfpdlsj0508@gmail.com>
2025-09-10 01:48:18 +0900

    feat: Swagger, OpenAiConfig

---
+++ src/main/java/com/spring/ai/tutorial/config/OpenAiConfig.java
@@ -0,0 +1,25 @@
+package com.spring.ai.tutorial.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class OpenAiConfig {
+
+    @Value("${spring.ai.openai.api-key}")
+    private String apiKey;
+
+    @Bean
+    public OpenAiApi openAiApi() {
+
+        log.info(apiKey);
+        return OpenAiApi.builder()
+                .apiKey(apiKey)
+                .build();
+    }
+
+}
---
+++ src/main/java/com/spring/ai/tutorial/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.spring.ai.tutorial.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI springOpenAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Spring AI Tutorial")
+                                .version("1.0")
+                                .description("Spring AI Tutorial")
+                );
+    }
+}
